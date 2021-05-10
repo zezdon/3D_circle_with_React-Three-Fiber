f6186f60d3a51c859b74ddb4dc50f13b80fb075d
Mats G <ed54res@hotmail.com>
2021-05-10 17:42:51 +0200

    1 Setup BP and display Cirles v2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import * as THREE from 'three';
+import { Canvas, useLoader } from 'react-three-fiber';
+import circleImg from './assets/circle.png';
+import { Suspense, useMemo } from 'react';
+
+//Code
+
+function Points() {
+  const imgTex = useLoader(THREE.TextureLoader, circleImg);
+ 
+  const count = 100
+  const sep = 3
+  let positions = useMemo(() => {
+    let positions = []
+
+    for (let xi = 0; xi < count; xi++) {
+      for (let zi = 0; zi < count; zi++) {
+        let x = sep * (xi - count / 2);
+        let z = sep * (zi - count / 2);
+        let y = 0;
+        positions.push(x, y, z);
+      }
+    }
+
+    return new Float32Array(positions);
+
+  }, [count, sep])
+
+  return(
+    <points>
+      <bufferGeometry attach="geometry">
+        <bufferAttribute
+          attachObject={['attributes', 'position']}
+          array={positions}
+          count={positions.length / 3}
+          itemSize={3}
+        />
+      </bufferGeometry>
+
+      <pointsMaterial
+        attach="material"
+        map={imgTex}
+        color={0x00AAFF}
+        size={0.5}
+        sizeAttenuation
+        transparent={false}
+        alphaTest={0.5}
+        opacity={1.0}
+      />
+    </points>
+  );
+}
+
+function AnimationCanvas() {
+  return (
+    <Canvas
+      colorManagement={false}
+      camera={{ position: [100, 10, 0], fov: 75 }}
+    >
+      <Suspense fallback={null}>
+        <Points />
+      </Suspense>
+    </Canvas>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="anim">
+      <Suspense fallback={<div>Loading...</div>}>
+        <AnimationCanvas />
+      </Suspense>
     </div>
   );
 }
